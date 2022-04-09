aed1d991abbbd1eb19e3343aa0b3d0e725535e77
Olivier Huin <olih@users.noreply.github.com>
2022-04-09 16:11:58 +0100

    Delete deprecated files

--- src/reaction/reactive.ts
+++
@@ -1,9 +0,0 @@
-import { BoardclothMessage } from '../message/messaging';
-
-export interface MessageSender {
-  send(message: BoardclothMessage): void;
-}
-
-export interface MessageReceiver {
-  onReceive(message: BoardclothMessage): Promise<void>;
-}
