fe341aecbf04ac18861e89d99fb393c8cd80668d
Olivier Huin <olih@users.noreply.github.com>
2022-04-20 09:25:38 +0100

    convert portable document to zod

--- src/portable-board-document.ts
+++ src/portable-board-document.ts
@@ -1,41 +1,51 @@
-type PortableColumn =
-  | {
-      kind: 'single';
-      flags: string[];
-      values: string[];
-    }
-  | {
-      kind: 'single-optional';
-      flags: string[];
-      values: (string | undefined)[];
-    }
-  | {
-      kind: 'multiple';
-      flags: string[];
-      values: string[][];
-    };
+import { z } from 'zod';
 
-interface PortableTable {
-  count: number;
-  flags: string[];
-  primaryKeys: string[];
-  columns: { [key: string]: PortableColumn };
-}
-type IntegrityStrategy = 'SHA-1' | 'Keccak'
+const portableSingle = z.object({
+  kind: z.literal('single'),
+  flags: z.string().array(),
+  values: z.string().array(),
+});
+
+const portableSingleOptional = z.object({
+  kind: z.literal('single-optional'),
+  flags: z.string().array(),
+  values: z.string().optional().array(),
+});
+
+const portableMultiple = z.object({
+  kind: z.literal('multiple'),
+  flags: z.string().array(),
+  values: z.string().array().array(),
+});
+
+const portableColumn = z.discriminatedUnion('kind', [
+  portableSingle,
+  portableSingleOptional,
+  portableMultiple,
+]);
+
+const portableTable = z.object({
+  count: z.number().int().positive(),
+  flags: z.string().array(),
+  primaryKeys: z.string().array().nonempty().max(12),
+  columns: z.record(portableColumn),
+});
+
+const integrity = z.object({
+  hashCode: z.string().optional(),
+  size: z.number().int().positive(),
+  strategy: z.string(),
+});
+const portableBoardDocument = z.object({
+  id: z.string(),
+  scopes: z.string().array(),
+  flags: z.string().array(),
+  metadata: z.record(z.string()),
+  prefixes: z.record(z.string()),
+  tables: z.record(portableTable),
+  integrity,
+});
 
 /**
  * @link https://cryptojs.gitbook.io/docs/
  */
-interface PortableBoardDocument {
-  id: string;
-  integrity: {
-
-    hashCode?: string;
-    size: number;
-  };
-  scopes: string[];
-  metadata: { [Key in string]: string };
-  prefixes: { [Key in string]: string };
-  flags: string[];
-  tables: { [Key in string]: PortableTable };
-}
